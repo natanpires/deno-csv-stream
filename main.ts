e30c5c690585bcd00c447b4fd06a1b3e548878c2
Natan Souza <52323199+natanpires@users.noreply.github.com>
2023-03-08 22:04:39 +0000

    feat: fetch csv data from url.

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { readableStreamFromReader } from "https://deno.land/std@0.140.0/streams/conversion.ts";
+import { serve } from 'https://deno.land/std@0.140.0/http/server.ts';
 
 const REGEX_ESCAPE = /(\r\n|\n|\r|\")/gm;
 
@@ -7,34 +6,38 @@ const csvToJSON = () => {
   let columns: string[] = [];
 
   const getHeader = (str: string): string[] => {
-    const cleaned = str.replace(REGEX_ESCAPE, "");
-    if (cleaned[cleaned.length - 1] == ",") {
-      return cleaned.substring(0, cleaned.length - 1).split(",");
+    const cleaned = str.replace(REGEX_ESCAPE, '');
+    if (cleaned[cleaned.length - 1] == ',') {
+      return cleaned.substring(0, cleaned.length - 1).split(',');
     }
-    return cleaned.split(",");
+    return cleaned.split(',');
   };
 
   return new TransformStream({
     transform(chunk, controller) {
-      const lines = chunk.split("\n");
+      const lines = chunk.split('\n');
+      let firstLineIdx = 0;
 
       if (!columns.length) {
-        columns = lines[1] !== "\r"
-          ? getHeader(lines.shift())
-          : getHeader(lines[lines.lastIndexOf("\r") + 1]);
+        if (lines[1] !== '\r') {
+          columns = getHeader(lines.shift());
+        } else {
+          firstLineIdx = lines.lastIndexOf('\r') + 1;
+          columns = getHeader(lines[firstLineIdx]);
+        }
       }
 
-      for (const line of lines) {
+      for (const line of lines.slice(firstLineIdx + 1)) {
         if (!line.length) continue;
 
-        const currentColumns = line.trimEnd().split(",");
+        const currentColumns = line.trimEnd().split(',');
         const currentItem: Record<string, unknown> = {};
         for (const columIndex in currentColumns) {
           const columnItem = currentColumns?.[columIndex];
-          if (!!columnItem) {
+          if (!!columnItem && columIndex) {
             currentItem[columns[columIndex]] = columnItem.replace(
               REGEX_ESCAPE,
-              "",
+              ''
             );
           }
         }
@@ -46,33 +49,50 @@ const csvToJSON = () => {
 };
 
 // HttpServer
-async function handler(_req) {
-    // Try opening the file
-    try {
-      const file = await Deno.open(
-          "./API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4901850.csv",
-        { read: true }
+async function handler(req) {
+  // Try opening the file
+  let data: Record<string, unknown>[] = [];
+  let finished = false;
+  try {
+    const response = await fetch(
+      req.url.replace(new URL(req.url).origin + '/', '')
+    );
+
+    const body = await response.body
+      ?.pipeThrough(new TextDecoderStream())
+      .pipeThrough(csvToJSON())
+      .pipeTo(
+        new WritableStream({
+          write: (chunk) => {
+            if (Object.keys(chunk).length) data.push(chunk);
+          },
+          close: () => {
+            finished = true;
+          },
+        })
       );
 
-      const fsStream: ReadableStream = readableStreamFromReader(file);
-
-      fsStream.pipeThrough(new TextDecoderStream())
-          .pipeThrough(csvToJSON())
-          .pipeTo(
-              new WritableStream({
-                  write: (data) => {
-                  console.log(data);
-                  },
-                  close: () => {},
-              }),
-          );
-      
-      // Build and send the response
-      return new Response(fsStream);
-    } catch {
-      // If the file cannot be opened, return a "404 Not Found" response
-      return new Response("404 Not Found", { status: 404 });
+    while (!finished) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
     }
+
+    return new Response(JSON.stringify(data));
+  } catch (error) {
+    // If the file cannot be opened, return a "404 Not Found" response
+    if (error instanceof Deno.errors.NotFound)
+      return new Response(
+        JSON.stringify({
+          success: false,
+          error: 'Unable to open the requested file',
+        }),
+        { status: 404 }
+      );
+
+    console.log(error);
+
+    // Return a "422 Unprocessable Entity" response
+    return new Response('Unable to process the request', { status: 422 });
+  }
 }
 
 serve(handler);
